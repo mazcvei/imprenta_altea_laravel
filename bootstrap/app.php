36084ffe32f85ea5b43e2d4b2c317e6e8fa5da30
MarioAV <mario.azcvei@hotmail.com>
2026-05-27 00:53:46 +0200

    admin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,11 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
+
+        $middleware->validateCsrfTokens(except: [
+            'stripe/webhook',
+            'stripe/*'
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
